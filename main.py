93bd0ca742053ef15cd64414b0acc75a4e3e85b3
lixiaofeng <lixiaofeng@100mj.com>
2024-05-31 15:46:52 +0800

    mode为none

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import eel
 import multiprocessing
 from engine.features import *
 from engine.command import *    #导入command的所有功能
-eel.init("www")
+
 def oth_thread():
      while True:
         #   print("i am a thread")
           eel.sleep(1)
 eel.spawn(oth_thread)
 def start():
+    eel.init("www")
     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
-    eel.start('index.html',mode='chrom',host='localhost',block=True)
+    eel.start('index.html',mode='None',host='localhost',block=True)
 
 if __name__ == '__main__':
      p1 = multiprocessing.Process(target=start)
